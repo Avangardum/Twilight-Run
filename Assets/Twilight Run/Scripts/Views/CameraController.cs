838a072627c612d5cc6f8ed7a02e99214744b328
Avangardum <yurykuz65@gmail.com>
2023-01-27 13:16:26 +0300

    Made camera movement

---
+++ Assets/Twilight Run/Scripts/Views/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Avangardum.TwilightRun.Views
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+        
+        private Vector3 _offset;
+        private float _yPosition;
+        
+        private void Awake()
+        {
+            _offset = transform.position - _target.position;
+            _yPosition = transform.position.y;
+        }
+        
+        private void LateUpdate()
+        {
+            var position = _target.position + _offset;
+            position.y = _yPosition;
+            transform.position = position;
+        }
+    }
+}
