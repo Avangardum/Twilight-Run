30a733c7a78b33224634d9d13f5e492380088735
Avangardum <yurykuz65@gmail.com>
2023-02-06 22:00:30 +0300

    Redesigned UI, added button click sound

---
+++ Assets/Twilight Run/Scripts/Views/ButtonClickSoundPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace Avangardum.TwilightRun.Views
+{
+    [RequireComponent(typeof(Button))]
+    public class ButtonClickSoundPlayer : MonoBehaviour
+    {
+        [SerializeField] private AudioClip _clickSound;
+        
+        private Button _button;
+        private AudioSource _audioSource;
+
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+            _audioSource = GameObject.Find("UI Audio Source").GetComponent<AudioSource>();
+            Assert.IsNotNull(_audioSource);
+            
+            _button.onClick.AddListener(PlaySound);
+        }
+
+        private void PlaySound()
+        {
+            _audioSource.PlayOneShot(_clickSound);
+        }
+    }
+}
