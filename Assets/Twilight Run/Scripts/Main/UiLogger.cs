8b56bc15c29bf41bf01fc1432e9846257c56737d
Avangardum <yurykuz65@gmail.com>
2023-02-24 18:59:17 +0300

    Fixed removing ads, added UI logger, changed version

---
+++ Assets/Twilight Run/Scripts/Main/UiLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Avangardum.TwilightRun.Main
+{
+    public class UiLogger : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _logText;
+        
+        private void Awake()
+        {
+            Application.logMessageReceived += OnLogMessageReceived;
+        }
+        
+        private void OnLogMessageReceived(string condition, string stackTrace, LogType type)
+        {
+            var color = type switch
+            {
+                LogType.Error => "red",
+                LogType.Assert => "red",
+                LogType.Exception => "red",
+                LogType.Warning => "yellow",
+                LogType.Log => "white",
+                _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+            };
+            _logText.text += $"<color=\"{color}\">{condition}</color>\n";
+        }
+    }
+}
